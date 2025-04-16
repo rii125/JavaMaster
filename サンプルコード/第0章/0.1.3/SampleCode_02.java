be7f9cd56ce81a50f5410d76ce0e5c434358dcbb
rii_125 <rii125.idd@gmail.com>
2025-04-17 07:18:46 +0900

    Create SampleCode_02.java

---
+++ サンプルコード/第0章/0.1.3/SampleCode_02.java
@@ -0,0 +1,14 @@
+public class SampleCode_01 {
+  public static void main(String[] args) {
+    methodA();
+    methodB();
+  }
+  public static void methodA() {
+    System.out.println("Hello ");
+    System.out.println("World!");
+  }
+  public static void methodB() {
+    System.out.print("Hello ");
+    System.out.print("World!");
+  }
+}
